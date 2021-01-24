174b6159a4055d893d1458bd33940407ded57fd2
juris124 <31391698+juris124@users.noreply.github.com>
2021-01-24 22:57:38 +0200

    pamainits nosaukums

--- Diena_6_lists/uzd3_1_g1.py
+++ Diena_6_lists/uzd3_1_g1.py
@@ -3,6 +3,7 @@ text = input("Ievadiet tekstu: ")
 words = text.split() # by default we split by whitespace, we could split by words as well
 for word in words:
     new_list.append(word[::-1].capitalize())
+print(f"Sākuma teksts: {text}")
 print(f"Apgrieztie vārdi: {new_list}")
 print(" ".join(new_list))
 
